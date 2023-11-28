570219046133340173f8b9f5fc317dd0b0b9afd4
Anton Chen <antonwchen@gmail.com>
2023-11-29 00:31:25 +0100

    Right click decrement

--- gbf-chest-tracker/src/lib/ChestItem.svelte
+++ gbf-chest-tracker/src/lib/ChestItem.svelte
@@ -8,8 +8,13 @@
   export let item_name = ""
   export let item_count = ""
 
-  function incrementCount(raid_tier, raid_name, chest_name, item_name) {
+  function incrementCount(event, aid_tier, raid_name, chest_name, item_name) {
     $DropStore[raid_tier][raid_name][chest_name][item_name]++
+  }
+
+  function decrementCount(event, raid_tier, raid_name, chest_name, item_name) {
+    event.preventDefault()
+    $DropStore[raid_tier][raid_name][chest_name][item_name] =  Math.max($DropStore[raid_tier][raid_name][chest_name][item_name]-1, 0)
   }
 
   function getTotal(raid_tier, raid_name, chest_name) {
@@ -20,7 +25,7 @@
 <div class="grid rounded grid-rows-5 grid-cols-2 items-center gap-0">
   <div class="row-span-3 col-span-2 text-center">
     <div class="relative">
-      <button on:click={() => incrementCount(raid_tier, raid_name, chest_name, item_name)}><img src={item_img} alt={item_img_alt} /></button>
+      <button on:click={(event) => incrementCount(event, raid_tier, raid_name, chest_name, item_name)} on:contextmenu={(event) => decrementCount(event, raid_tier, raid_name, chest_name, item_name)}><img src={item_img} alt={item_img_alt} /></button>
       <div class="absolute pointer-events-none mb-2 mr-2 bottom-0 right-0 opacity-90"><p class="text-3xl select-none text-white font-bold">{item_count}</p></div>
     </div>
   </div>
